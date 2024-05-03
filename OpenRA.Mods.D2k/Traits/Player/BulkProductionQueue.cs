057c16292fd528be5f2bf517e8c37280bb669fde
N.N <porenut@centrum.sk>
2024-05-03 17:53:24 +0200

    multidelivery

--- OpenRA.Mods.D2k/Traits/Player/BulkProductionQueue.cs
+++ OpenRA.Mods.D2k/Traits/Player/BulkProductionQueue.cs
@@ -11,10 +11,12 @@
 
 using System.Collections.Generic;
 using System.Linq;
+using System;
 using OpenRA.Mods.Common;
 using OpenRA.Mods.Common.Traits;
 using OpenRA.Primitives;
 using OpenRA.Traits;
+using System.Xml.Schema;
 
 namespace OpenRA.Mods.D2k.Traits
 {
@@ -26,16 +28,8 @@ namespace OpenRA.Mods.D2k.Traits
 		[Desc("If you build more actors of the same type,", "the same queue will get its build time lowered for every actor produced there.")]
 		public readonly bool SpeedUp = false;
 
-		[Desc("Every time another production building of the same queue is",
-			"constructed, the build times of all actors in the queue",
-			"decreased by a percentage of the original time.")]
-		public readonly int[] BuildTimeSpeedReduction = { 100, 85, 75, 65, 60, 55, 50 };
-
-		[Desc("Minimum amount of units that can be produced at one time")]
-		public readonly int MinimumOrders = 3;
-
-		[Desc("Time it takes to process the order.")]
-		public readonly int BuildTime = 1000;
+		[Desc("Maximum deliver capacity")]
+		public readonly int MaxCapacity = 3;
 
 		public override object Create(ActorInitializer init) { return new BulkProductionQueue(init, this); }
 	}
@@ -47,7 +41,8 @@ namespace OpenRA.Mods.D2k.Traits
 		readonly Actor self;
 		readonly BulkProductionQueueInfo info;
 
-		protected readonly List<ActorInfo> ReadyForDelivery = new List<ActorInfo>();
+		int totalOrderValue = 0;
+		protected readonly List<KeyValuePair<ActorInfo, TypeDictionary>> ActorsReadyForDelivery = new();
 
 		public BulkProductionQueue(ActorInitializer init, BulkProductionQueueInfo info)
 			: base(init, info)
@@ -110,10 +105,12 @@ namespace OpenRA.Mods.D2k.Traits
 			// Some units may request a specific production type, which is ignored if the AllTech cheat is enabled
 			var type = developerMode.AllTech ? Info.Type : (bi.BuildAtProductionType ?? Info.Type);
 
-			var producers = self.World.ActorsWithTrait<Production>()
+			var producers = self.World.ActorsWithTrait<ProductionStarport>()
 				.Where(x => x.Actor.Owner == self.Owner
 					&& !x.Trait.IsTraitDisabled
-					&& x.Trait.Info.Produces.Contains(type));
+					&& x.Trait.Info.Produces.Contains(type))
+					.OrderByDescending(x => x.Actor.Trait<PrimaryBuilding>().IsPrimary)
+					.ThenByDescending(x => x.Actor.ActorID);
 
 			var anyProducers = false;
 			foreach (var p in producers)
@@ -129,10 +126,28 @@ namespace OpenRA.Mods.D2k.Traits
 				};
 
 				var item = Queue.First(i => i.Done && i.Item == unit.Name);
-				if (p.Trait.Produce(p.Actor, unit, type, inits, item.TotalCost))
+				if (ActorsReadyForDelivery.Count < info.MaxCapacity)
 				{
+					ActorsReadyForDelivery.Add(new KeyValuePair<ActorInfo, TypeDictionary>(unit, inits));
+					Console.WriteLine("pocet:" + ActorsReadyForDelivery.Count);
+					totalOrderValue += item.TotalCost;
 					EndProduction(item);
-					return true;
+				}
+
+				if (ActorsReadyForDelivery.Count == info.MaxCapacity)
+				{
+					Console.WriteLine("starting delivery");
+					if (p.Trait.DeliverOrder(p.Actor, ActorsReadyForDelivery, type, totalOrderValue))
+					{
+						EndProduction(item);
+						totalOrderValue = 0;
+						//ActorsReadyForDelivery.Clear();
+						return true;
+					}
+				}
+				else
+				{
+					return false;
 				}
 			}
 
